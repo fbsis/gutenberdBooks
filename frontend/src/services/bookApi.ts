c4b603b6b7614b4d21c4f2a5c5fa7f2441315ab7
Felipe Braga <git@fbsis.com>
2025-04-04 22:29:31 -0300

    feat: adds book fetching functionality
    Introduces a service to fetch book data from the API.
    This allows the frontend to retrieve book information based on a given ID.

---
+++ frontend/src/services/bookApi.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { Book } from '../types/Book';
+
+const api = axios.create({
+  baseURL: 'http://localhost:4000'
+});
+
+export const fetchBook = async (bookId: string): Promise<Book> => {
+  const response = await api.get(`/books/${bookId}`);
+  return response.data;
+}; 
